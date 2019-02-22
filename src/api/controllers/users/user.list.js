62062e52f578d2b7366a1a9b8ac90015cb5fe7f5
François-Luc Haghenbeek <francois_luc.haghenbeek@utt.fr>
2019-02-22 19:56:16 +0100

    fix endpoints for list users

--- src/api/controllers/users/user.list.js
+++ src/api/controllers/users/user.list.js
@@ -1,3 +1,6 @@
+const log = require('../../utils/log')(module)
+const errorHandler = require('../../utils/errorHandler')
+
 /**
  * GET /user/list
  *
@@ -10,13 +13,21 @@ module.exports = app => {
   app.get('/user/list', async (req, res) => {
     try {
       const { User } = req.app.locals.models
-
+      console.log('TON PADRE')
       const users = await User.findAll({
         order: [['town', 'ASC']]
       })
+
+      let usersFinal = users.map(user =>{
+        return{
+          name: `${user.firstName}.${user.lastName.charAt(0).toUpperCase()} ${user.nickName ? user.nickName : '' }`,
+          town: user.town
+        }
+      })
+      console.log('TON PERE', usersFinal)
       return res
         .status(200)
-        .json(users)
+        .json(usersFinal)
         .end()
     } catch (err) {
       errorHandler(err, res)
