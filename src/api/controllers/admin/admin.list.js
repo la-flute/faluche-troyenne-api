963c4ae3c36b31b677975a6660277a2c53c245d8
François-Luc Haghenbeek <francois_luc.haghenbeek@utt.fr>
2019-02-26 18:10:33 +0100

    testing admin/list

--- src/api/controllers/admin/admin.list.js
+++ src/api/controllers/admin/admin.list.js
@@ -18,21 +18,21 @@ module.exports = app => {
       const { User, Order } = req.app.locals.models
       const users = await User.findAll({
         // attributes: ['id', 'lastName', 'firstName', 'nickName', 'town'],
-        include: [{model: Order}],
+        include: [{model: Order, attributes:['paid']}],
         order: [['town', 'ASC']]
       })
 
-      // let usersFinal = users.map(user =>{
-      //   return{
-      //     lastName: user.lastName.charAt(0).toUpperCase(),
-      //     firstName: user.firstName,
-      //     nickName: user.nickName,
-      //     studies: user.studies,
-      //     town: user.town,
-      //     folklore: user.folklore,
-      //     order: user.order
-      //   }
-      // })
+      let usersFinal = users.map(user =>{
+        return{
+          lastName: user.lastName.charAt(0).toUpperCase(),
+          firstName: user.firstName,
+          nickName: user.nickName,
+          studies: user.studies,
+          town: user.town,
+          folklore: user.folklore,
+          orders: user.orders.paid
+        }
+      })
       return res
         .status(200)
         .json(users)
