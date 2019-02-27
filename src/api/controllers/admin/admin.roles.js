a03a5b5ea30c76fea82c5bb48209d2d7f381b0a7
François-Luc Haghenbeek <francois_luc.haghenbeek@utt.fr>
2019-02-27 19:01:59 +0100

    setting up roles management

--- src/api/controllers/admin/admin.roles.js
+++ src/api/controllers/admin/admin.roles.js
@@ -3,12 +3,22 @@ const errorHandler = require('../../utils/errorHandler')
 const isAuth = require('../../middlewares/isAuth')
 const isAdmin = require('../../middlewares/isAdmin')
 
+/**
+ * GET /admin/listRoles
+ *
+ * Response:
+ * [
+ *    { id, name, firstname, lastname, nickName, town }, ...
+ * ]
+ */
 module.exports = app => {
-  app.get('admin/listRoles', async (req, res) => {
+  app.get('/admin/listRoles', async (req, res) => {
     try {
       const { User, Permission } = req.app.locals.models
       const users = await User.findAll({
-        include: [Permission]
+        attributes: ['id', 'lastName', 'firstName', 'nickName', 'town'],
+        include: [{model:Permission, attributes: ['admin', 'treso', 'bureau', 'write']}],
+        order: [['town', 'ASC'], ['lastName', 'ASC']]
       })
       return res
         .status(200)
